13bce27cdadb7f0ba39b24c63879384904114347
Achowdhury2019 <chowdhuryaisha089@gmail.com>
2021-03-02 18:38:56 -0500

    first commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -0,0 +1,118 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.parceler.Parcels;
+
+import okhttp3.Headers;
+
+public class ComposeActivity extends AppCompatActivity {
+
+    public static final String TAG = "ComposeActivity";
+    public static final int MAX_TWEET_LENGTH = 140;
+
+    EditText etCompose;
+    TextView textView;
+    Button btnTweet;
+
+    TwitterClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+
+        client = TwitterApp.getRestClient(this);
+
+        etCompose = findViewById(R.id.etCompose);
+        textView = findViewById(R.id.textView);
+        btnTweet = findViewById(R.id.btnTweet);
+
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String text = etCompose.getText().toString();
+                int symbols = text.length();
+                textView.setText(symbols + "/150");
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tweetContent = etCompose.getText().toString();
+                if(tweetContent.isEmpty()){
+                    Toast.makeText(ComposeActivity.this,"Sorry, your tweet can not be empty :)",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(tweetContent.length() > MAX_TWEET_LENGTH){
+                    Toast.makeText(ComposeActivity.this,"Sorry, your tweet is too long!",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
+                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.i(TAG, "onSuccess to publish tweet");
+                        try {
+                            Tweet tweet = Tweet.fromJson(json.jsonObject);
+                            Log.i(TAG, "Published tweet syas: " + tweet.body);
+                            Intent intent = new Intent();
+                            intent.putExtra("tweet", Parcels.wrap(tweet));
+                            setResult(RESULT_OK, intent);
+                            finish();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.e(TAG, "onFailure to publish tweet", throwable);
+                    }
+                });
+
+            }
+        });
+
+        // Define ActionBar object
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        // Set BackgroundDrawable
+        actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.twitter_blue)));
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_icon_twitter);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+    }
+}
